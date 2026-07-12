b3845e36f2ebf959d4c701550f800c131981f701
Priyansh Aggarwal <priyansh@Priyanshs-MacBook-Air.local>
2026-07-12 15:06:49 +0530

    load country

--- spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
+++ spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -1,13 +1,30 @@
 package com.cognizant.spring_learn;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-@SpringBootApplication
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class SpringLearnApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringLearnApplication.class, args);
-	}
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(SpringLearnApplication.class);
 
+    public static void displayCountry() {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("country.xml");
+
+        Country country =
+                context.getBean("country", Country.class);
+
+        LOGGER.debug("Country : {}", country);
+    }
+
+    public static void main(String[] args) {
+
+        displayCountry();
+
+    }
 }
