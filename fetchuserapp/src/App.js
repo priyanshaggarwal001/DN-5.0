ee4f1223578c0460864485c02ee9114183ff5fa0
Priyansh Aggarwal <priyansh@Priyanshs-MacBook-Air.local>
2026-07-22 09:37:43 +0530

    React17

---
+++ fetchuserapp/src/App.js
@@ -0,0 +1,17 @@
+import Getuser from "./Getuser";
+
+function App() {
+
+    return (
+
+        <div>
+
+            <Getuser />
+
+        </div>
+
+    );
+
+}
+
+export default App;
---
+++ fetchuserapp/src/Getuser.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+
+class Getuser extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: "",
+            firstname: "",
+            image: ""
+        };
+    }
+
+    async componentDidMount() {
+
+        const response = await fetch("https://api.randomuser.me/");
+        const data = await response.json();
+
+        this.setState({
+            title: data.results[0].name.title,
+            firstname: data.results[0].name.first,
+            image: data.results[0].picture.large
+        });
+    }
+
+    render() {
+
+        return (
+
+            <div style={{ textAlign: "center", marginTop: "30px" }}>
+
+                <h2>Random User Details</h2>
+
+                <img
+                    src={this.state.image}
+                    alt="User"
+                    width="200"
+                    height="200"
+                />
+
+                <h3>Title : {this.state.title}</h3>
+
+                <h3>First Name : {this.state.firstname}</h3>
+
+            </div>
+
+        );
+
+    }
+
+}
+
+export default Getuser;
